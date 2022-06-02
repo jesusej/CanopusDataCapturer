e460021ac8305b84402e8ec8c5a7a463c50bfc36
mikeproxd1 <a01252852@tec.mx>
2022-06-01 19:24:29 -0700

    Balance de Comprobacion mod

--- src/Components/TrialBalance/TrialBalance.js
+++ src/Components/TrialBalance/TrialBalance.js
@@ -3,17 +3,17 @@ import TrialBalanceIterable from './TrialBalanceIterable'
 
 function TrialBalance({data}) {
   return (
-    <div>
-      <table>
+    <div className='content'>
+      <table class= "table" style={{width:"1500px"}}>
         <thead>
-          <th id="code">Cuenta</th>
-          <th id='name'>Nombre</th>
-          <th id='initial-balance-debtor'>Saldos iniciales deudor</th>
-          <th id='initial-balance-creditor'>Saldos iniciales acreedor</th>
-          <th id='charge'>Cargos</th>
-          <th id='deposit'>Abonos</th>
-          <th id='actual-balance-debtor'>Saldos actuales deudor</th>
-          <th id='actual-balance-creditor'>Saldos actuales acreedor</th>
+          <th scope="col"  id="code">Cuenta</th>
+          <th scope="col"  id='name'>Nombre</th>
+          <th scope="col"  id='initial-balance-debtor'>Saldos iniciales deudor</th>
+          <th scope="col"  id='initial-balance-creditor'>Saldos iniciales acreedor</th>
+          <th scope="col"  id='charge'>Cargos</th>
+          <th scope="col"  id='deposit'>Abonos</th>
+          <th scope="col"  id='actual-balance-debtor'>Saldos actuales deudor</th>
+          <th scope="col"  id='actual-balance-creditor'>Saldos actuales acreedor</th>
         </thead>
         <tbody>
           {data.accounts.map((account) => {
@@ -23,13 +23,13 @@ function TrialBalance({data}) {
         </tbody>
         <tr>
           <td></td>
-          <td headers='name'>Sumas iguales:</td>
-          <td headers='initial-balance-debtor'>{data.sums.initialBalanceSums.debtor}</td>
-          <td headers='initial-balance-creditor'>{data.sums.initialBalanceSums.creditor}</td>
-          <td headers='charge'>{data.sums.charge}</td>
-          <td headers='deposit'>{data.sums.deposit}</td>
-          <td headers='actual-balance-debtor'>{data.sums.actualBalanceSums.debtor}</td>
-          <td headers='actual-balance-creditor'>{data.sums.actualBalanceSums.creditor}</td>
+          <td scope="row" headers='name'>Sumas iguales:</td>
+          <td scope="row" headers='initial-balance-debtor'>{data.sums.initialBalanceSums.debtor}</td>
+          <td scope="row" headers='initial-balance-creditor'>{data.sums.initialBalanceSums.creditor}</td>
+          <td scope="row" headers='charge'>{data.sums.charge}</td>
+          <td scope="row" headers='deposit'>{data.sums.deposit}</td>
+          <td scope="row" headers='actual-balance-debtor'>{data.sums.actualBalanceSums.debtor}</td>
+          <td scope="row" headers='actual-balance-creditor'>{data.sums.actualBalanceSums.creditor}</td>
         </tr>
       </table>
     </div>
